38b3b8e86b34522d8ed06b868709612942f42c72
ivan pinton <ivan.pinton@gmail.com>
2025-10-27 19:31:41 +0100

    added sidebar

--- app.py
+++ app.py
@@ -2,7 +2,22 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-st.write("Hello world!")
+
+st.write("""
+# SQL SRS
+Spaced Repetition System SQL practice
+""")
+
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review ?",
+        ["Joins", "GroupBy", "Window Functions"],
+        index=None,
+        placeholder="Select a theme..."
+    )
+    st.write("You selected:", option)
+
+
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
 df = pd.DataFrame(data)
 
