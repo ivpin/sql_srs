093603e752697c74df0e72cf9a36e0d07862d0cf
ivan pinton <ivan.pinton@gmail.com>
2025-10-27 17:10:47 +0100

    added sidebar

--- app.py
+++ app.py
@@ -3,28 +3,7 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-csv = '''
-beverage, price
-orange juice,2.5
-Expresso,2
-Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
-
-csv2 = '''
-food_item, food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
-
-answer = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+solution = duckdb.sql(answer).df()
 
 with st.sidebar:
     option = st.selectbox(
@@ -35,18 +14,3 @@ with st.sidebar:
     )
     st.write("You selected:", option)
 
-st.header("enter your code")
-query = st.text_area(label="votre code SQL ici", key="user_input")
-if query:
-    result = duckdb.sql(query).df()
-    st.write(f"Vous avez entré la query suivante : {query}")
-    st.dataframe(result)
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-
-with tab3:
-    st.write(answer)
