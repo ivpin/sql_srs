0dc91c608510e1565de1321be340f7cb06cc08aa
ivan pinton <ivan.pinton@gmail.com>
2025-10-28 18:53:39 +0100

    added ci and .gitignore and cleanup code with black

--- app.py
+++ app.py
@@ -1,30 +1,31 @@
+# pylint: disable =missing-module-docstring
 import io
-import streamlit as st
-import pandas as pd
+
 import duckdb
+import pandas as pd
+import streamlit as st
 
-
-csv = '''
-beverage, price
+CSV = """
+beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
-food_item, food_price
+CSV2 = """
+food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+"""
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = duckdb.sql(answer).df()
+solution_df = duckdb.sql(ANSWER_STR).df()
 
 
 with st.sidebar:
@@ -32,7 +33,7 @@ with st.sidebar:
         "What would you like to review ?",
         ["Joins", "GroupBy", "Window Functions"],
         index=None,
-        placeholder="Select a theme..."
+        placeholder="Select a theme...",
     )
     st.write("You selected:", option)
 
@@ -43,6 +44,20 @@ if query:
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
+    if len(result.columns) != len(solution_df.columns):
+        st.write("Some columns are missing")
+
+    try:
+        result = result[solution_df.columns]
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -51,7 +66,7 @@ with tab2:
     st.write("table: food_items")
     st.dataframe(food_items)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3:
-    st.write(answer)
+    st.write(ANSWER_STR)
