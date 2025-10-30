7b42ec5cb397540e8091600bf373c4c723f069be
ivan pinton <ivan.pinton@gmail.com>
2025-10-30 17:46:50 +0100

    comparison with real answer

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ con = duckdb.connect(
     database="data/exercises_sql_tables.duckdb", read_only=False
 )  # connexion à une database
 
-# solution_df = duckdb.sql(ANSWER_STR).df()
+
 
 
 with st.sidebar:
@@ -23,32 +23,37 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+    solution_df = con.execute(answer).df()
 
 st.header("enter your code")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_difference = result.shape[0] - solution_df.shape[0]
-#     if n_lines_difference != 0:
-#         st.write(
-#             f"result has a {n_lines_difference} lines difference with the solution_df"
-#         )
-#
+
+    try:
+        result = result[solution_df.columns]
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
-#
+
 with tab2:
     exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
     for table in exercise_tables:
         st.write(f"table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()
         st.dataframe(df_table)
+
         # st.dataframe(beverages)
 #     st.write("table: food_items")
 #     st.dataframe(food_items)
@@ -56,7 +61,4 @@ with tab2:
 #     st.dataframe(solution_df)
 #
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open(f"answers/{exercise_name}.sql", "r") as f:
-        answer = f.read()
     st.write(answer)
